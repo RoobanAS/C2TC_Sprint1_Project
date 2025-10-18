4680af0a810f2135783c4ebfa22a29b6bb09fa21
RoobanAS <asrrsanthamary@gmail.com>
2025-10-18 14:39:55 +0530

    Project Updated

--- placement/src/main/java/com/tnsif/placement/config/StudentConfig.java
+++ placement/src/main/java/com/tnsif/placement/config/StudentConfig.java
@@ -1,5 +1,5 @@
 package com.tnsif.placement.config;
-import org.springframework.context.annotation.Configuration; 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
